ad19ec57c5956529f529c39690bb88463b097308
Loic CHETY <lchety@e3r12p22.42.fr>
2016-11-09 17:09:02 +0100

    lib do.h

---
+++ inc_test/libido.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libido.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lchety <lchety@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/10/11 16:02:23 by lchety            #+#    #+#             */
+/*   Updated: 2016/11/09 13:29:27 by lchety           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBIDO_H
+#define LIBIDO_H
+
+#define TEST(nb, good)	printf("Test %d %s\n", nb,good);
+#define MALLOC_CHECK(ptr)	if(ptr == NULL) 		\
+							{						\
+								do_malloc_failed(); \
+								return (0);			\
+							}
+
+#include <string.h>
+#include <stdio.h>
+#include "libft.h"
+
+
+void do_malloc_failed(void);
+
+#endif
